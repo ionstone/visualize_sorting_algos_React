5887c498a2c6b3a87c1c2e064e761a9d3f6984dd
Your Name <visheshyadav2001@gmail.com>
2023-07-05 22:10:32 +0530

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+function Footer() {
+	return (
+		<>
+			<div className="bottom-0 w-full bg-[#222831]">
+				<h1 className="text-[#00ADB5] text-xl text-center pt-4">
+                <p>
+						Created by{" "}
+						<a
+							href="https://www.linkedin.com/in/vishesh-yadav-0a4841125/"
+							target="blank"
+						>
+							Vishesh Yadav
+						</a>
+					</p>
+					
+				</h1>
+			</div>
+		</>
+	);
+}
+
+export default Footer;
